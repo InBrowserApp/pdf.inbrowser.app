97e6dd32c9296a9ec50e1b71f10a7453806ddf3b
Seedgou <i@zczc.cz>
2023-02-12 05:52:51 +0000

    add remove owner password view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import RemoveOwnerPasswordView from "../views/RemoveOwnerPasswordView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/tools/remove-owner-password",
+      name: "remove-owner-password",
+      component: RemoveOwnerPasswordView,
     },
   ],
 });
---
+++ src/views/RemoveOwnerPasswordView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <n-h1 prefix="bar" align-text>Remove Owner Password</n-h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH1 } from "naive-ui";
+</script>
