f6f129c315d994ca83768cbf6c019a7f39a593b0
Seedgou <i@zczc.cz>
2023-02-12 09:32:47 +0000

    add tools list in homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>Home</main>
+  <main>
+    <n-h2 prefix="bar" align-text>Tools List</n-h2>
+    <router-link
+      to="/tools/remove-owner-password"
+      #="{ navigate, href }"
+      custom
+    >
+      <n-button tag="a" :href="href" @click="navigate" text>
+        <template #icon>
+          <n-icon :component="Key16Regular" />
+        </template>
+        Remove Owner Password
+      </n-button>
+    </router-link>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { NH2, NButton, NIcon } from "naive-ui";
+import Key16Regular from "@vicons/fluent/Key16Regular";
+</script>
