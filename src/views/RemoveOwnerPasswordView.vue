e5a34b89b04d96891383306badc1cb2accadafb1
Seedgou <i@zczc.cz>
2023-02-12 08:55:59 +0000

    add upload component

--- src/views/RemoveOwnerPasswordView.vue
+++ src/views/RemoveOwnerPasswordView.vue
@@ -1,19 +1,24 @@
 <template>
   <main>
     <n-h1 prefix="bar" align-text>Remove Owner Password</n-h1>
+    <PDFUpload @upload:file="handlePDFUpload" />
   </main>
 </template>
 
 <script setup lang="ts">
 import { removeOwnerPassword } from "@/utils/qpdf/remove-owner-password";
 import { NH1 } from "naive-ui";
-import { onMounted } from "vue";
+import PDFUpload from "@/components/pdf/upload/PDFUpload.vue";
 
-onMounted(async () => {
-  const res = await fetch("/test.pdf");
-  const blob = await res.blob();
+const handlePDFUpload = async (file: File) => {
+  const newBlob = await removeOwnerPassword(file);
+  const url = URL.createObjectURL(newBlob);
+  const aTag = document.createElement("a");
 
-  const newBlob = await removeOwnerPassword(blob);
-  console.log(newBlob);
-});
+  aTag.href = url;
+  aTag.download = file.name;
+  aTag.click();
+  URL.revokeObjectURL(url);
+  aTag.remove();
+};
 </script>
