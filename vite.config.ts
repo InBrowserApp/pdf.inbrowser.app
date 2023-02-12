b7dd237ea1c862c1bd87da242a5a3e50cea46907
Seedgou <i@zczc.cz>
2023-02-12 10:24:02 +0000

    add vite pwa and icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,45 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        "favicon.ico",
+        "favicon-dark.ico",
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+      ],
+      workbox: {
+        globPatterns: ["assets/*"],
+        maximumFileSizeToCacheInBytes: 10000000,
+      },
+      manifest: {
+        name: "PDF InBrowser.App",
+        short_name: "PDF",
+        description:
+          "PDF InBrowser.App is a tool to handle PDFs. Fully runs in your browser, no data is sent to the server. Fast, secure, and offline capable.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
